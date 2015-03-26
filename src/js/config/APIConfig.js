141a658493e6b89d241ab52d401a5befdbe79a2d
Yongzhi Huang <yhuang@fastcompany.com>
2015-03-26 13:36:54 -0400

    simple config system

---
+++ src/js/config/APIConfig.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    API_BASE: 'http://mywebsite.com/api'
+};
+
---
+++ src/js/config/AppConfig.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    SITE_NAME: 'FluxZ-YARSK',
+    DOMAIN: 'http://yongzhihuang.com/fluxzyarsk',
+    CONTACT_EMAIL: 'yongzhihuang@yahoo.com'
+};
+
